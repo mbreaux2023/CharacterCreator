f817d202c15f1c31e9f820c2b35334cab6f01cb1
mbreaux2023 <mbreaux2023@chadwickschool.org>
2021-12-08 13:00:03 -0800

    feat add armor shop

--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    public SpriteRenderer WeaponRenderer;
     public SpriteRenderer ArmorRenderer;
     // Start is called before the first frame update
     public void SetArmor(Sprite newSprite)
     {
         ArmorRenderer.sprite = newSprite;
+    }
+
+    public void SetWeapon(Sprite newSprite)
+    {
+        WeaponRenderer.sprite = newSprite;
     }
     void Start()
     {
